3e55140536929f9262bb24af0878b579ad5c030c
Zaid Ibaisi <zed65pro@hotmail.com>
2023-05-19 18:14:43 +0400

    message fix

--- src/routes/messages.js
+++ src/routes/messages.js
@@ -1,7 +1,7 @@
 // routes/messages.js
 const express = require("express");
 const router = express.Router();
-const Message = require("../models/message");
+const Message = require("../models/Message");
 
 // Create a new message
 router.post("/", async (req, res) => {
